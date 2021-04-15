c0b4cbf4ed7449428f83221ee8732d002691559d
Rahul Prabhu <cupertinorp@gmail.com>
2021-04-15 14:08:40 +0530

    Matrices and Berlekamp Massey

---
+++ numerical/bm.hpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Source: ecnerwala
+// Complexity: O(n^2)
+// Given a sequence a_i, find the shortest possible linear recurrence p s.t.:
+// a_i = \sum_{j=1}^m a_{i-j}p_j
+template <typename num>
+std::vector<num> BerlekampMassey(const std::vector<num>& s) {
+  int n = int(s.size()), L = 0, m = 0;
+  std::vector<num> C(n), B(n), T;
+  C[0] = B[0] = 1;
+
+  num b = 1;
+  for(int i = 0; i < n; i++) { ++m;
+    num d = s[i];
+    for (int j = 1; j <= L; j++) d += C[j] * s[i - j];
+    if (d == 0) continue;
+    T = C; num coef = d / b;
+    for (int j = m; j < n; j++) C[j] -= coef * B[j - m];
+    if (2 * L > i) continue;
+    L = i + 1 - L; B = T; b = d; m = 0;
+  }
+
+  C.resize(L + 1); C.erase(C.begin());
+  for (auto& x : C) {
+    x = -x;
+  }
+  return C;
+}
+
+// Source: ecnerwala
+// Complexity: O(m^2 log k)
+// Given the sequence a, and the linear recurrence p, find the kth term in the
+// linear recurrence
+// S = a, tr = p
+// Usage:
+// tr = BerlekampMassey(S)
+// ans = linearRec(S,tr,k)
+template <typename num>
+num linearRec(const std::vector<num>& S, const std::vector<num>& tr, long long k) {
+  int n = int(tr.size());
+  assert(S.size() >= tr.size());
+
+  auto combine = [&](std::vector<num> a, std::vector<num> b) {
+    std::vector<num> res(n * 2 + 1);
+    for (int i = 0; i <= n; i++) for (int j = 0; j <= n; j++) res[i + j] += a[i] * b[j];
+    for (int i = 2 * n; i > n; --i) for (int j = 0; j < n; j++)
+      res[i - 1 - j] += res[i] * tr[j];
+    res.resize(n + 1);
+    return res;
+  };
+
+  std::vector<num> pol(n + 1), e(pol);
+  pol[0] = e[1] = 1;
+
+  for (++k; k; k /= 2) {
+    if (k % 2) pol = combine(pol, e);
+    e = combine(e, e);
+  }
+
+  num res = 0;
+  for (int i = 0; i < n; i++) res += pol[i + 1] * S[i];
+  return res;
+}
---
+++ numerical/matrix.hpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<class T>
+struct Mat{
+  int n,m;
+  vector<vector<T>> d;
+
+  Mat(): n(0), m(0){}
+
+  Mat(int _n, int _m): n(_n), m(_m) {
+    d = vector<vector<T>>(n,vector<T>(m));
+  }
+
+  Mat(const vector<vector<T>>& v): Mat((int)v.size(),(int)v[0].size()){
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++)
+        d[i][j] = v[i][j];
+  }
+
+  vector<T>& operator[] (int index) {
+    return d[index];
+  }
+
+  Mat operator +(Mat other) {
+    assert(n == other.n && m == other.m);
+    Mat res = Mat(n,m);
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++)
+        res.d[i][j] = d[i][j] + other.d[i][j];
+    return res;
+  }
+
+  Mat operator -(Mat other) {
+    assert(n == other.n && m == other.m);
+    Mat res = Mat(n,m);
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++)
+        res.d[i][j] = d[i][j] - other.d[i][j];
+    return res;
+  }
+
+  Mat operator *(Mat other) {
+    assert(m == other.n);
+    Mat prod = Mat(n,other.m);
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++)
+        for (int k = 0; k < other.m; k++)
+          prod.d[i][k] += d[i][j] * other.d[j][k];
+    return prod;
+  }
+
+  Mat operator ^(long long p)
+  {
+    assert(p >= 0);
+    Mat res(n,n), base(*this);
+    for (int i = 0; i < n; i++)
+      res.d[i][i] = 1;
+    for (;p; p /= 2) {
+      if (p&1) res = res*base;
+      base = base*base;
+    }
+    return res;
+  }
+
+  // Get the determinant of the matrix in O(N^3)
+  T det() {
+    assert(n == m);
+    T prod = 1;
+    Mat st(n,n);
+    st.d = d;
+    auto elim = [this](int a, int c){
+      T x = d[c][a];
+      for (int i = a; i < n; i++)
+        d[c][i] -= x*d[a][i];
+    };
+    for (int i = 0; i < n; i++){
+      bool done = false;
+      for (int j = i; j < n; j++){
+        if (d[j][i] != 0) {
+          done = 1;
+          if (j != i)
+            swap(d[j], d[i]);
+          if ((j-i)%2==1)
+            prod *= -1;
+          prod = prod*d[i][i];
+          T x = d[i][i].inv();
+          for (int k = i; k < n; k++)
+            d[i][k] *= x;
+          for (int k = i+1; k < n; k++)
+            elim(i,k);
+        }
+      }
+      if (!done) return 0;
+    }
+    d = st.d;
+    return prod;
+  }
+};
