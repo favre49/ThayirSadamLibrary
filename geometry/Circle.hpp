cd682d1d7db4ebc4918c25d5f6473bea6206bb0f
Rahul Prabhu <cupertinorp@gmail.com>
2021-04-16 07:33:56 +0530

    Fixed Circle template

--- geometry/Circle.hpp
+++ geometry/Circle.hpp
@@ -3,13 +3,12 @@ using namespace std;
 
 #include "Point.hpp"
 
-// Implementations from KACTL, untested
 template<class T>
 struct TCircle {
   typedef TCircle C;
   typedef TPoint<T> P;
-  static constexpr T eps = static_cast<T>(1e-9);
-  static constexpr T PI = atan(1)*4;
+  static constexpr T eps = static_cast<T>(1e-8);
+  static constexpr T PI = 3.14159265358979323846;
   P c;
   T r;
   TCircle(T x, T y, T rad):c(x,y),r(rad) {}
@@ -17,21 +16,22 @@ struct TCircle {
   T area() const { return PI*r*r; }
   T perimeter() const { return 2*PI*r; }
   T diameter() const { return 2*r; }
-
+ 
   // Returns true if there is an intersection
   bool intersect(C& other, pair<P,P>& out) {
-    P delta = other.c-c;
-    assert(delta.x || delta.y || r != other.r);
-    if (!delta.x && !delta.y) return false;
-    T rt = r+other.r, d2 = delta.dist2();
-    T p = (d2+r*r-other.r*other.r)/(2.0*d2);
-    T h2 = r*r-p*p*d2;
-    if (d2 > rt*rt || h2 < 0) return false;
-    P mid = c+delta*p,per = delta.perp()*sqrt(h2/d2);
-    out = {mid+per,mid-per};
+    if (c == other.c) {
+      assert(abs(r-other.r) > eps);
+      return false;
+    }
+    P vec = other.c-c;
+    T d2 = vec.dist2(), sum = r + other.r, dif = r - other.r,
+      p = (d2+r*r - other.r*other.r)/(d2*2), h2 = r*r-p*p*d2;
+    if (sum*sum < d2 || dif*dif > d2) return false;
+    P mid = c + vec*p, per = vec.perp() * sqrt(fmax(0,h2)/d2);
+    out = {mid+per, mid - per};
     return true;
   }
-
+ 
   // Returns a pair of two points on the circle whos tangents intersect at p
   // If p is inside the circle, NAN.
   // First point is the one to the right as seen from p towards the center
@@ -41,6 +41,5 @@ struct TCircle {
     return make_pair(c+a*x+a.perp()*y,c+a*x-a.perp()*y);
   }
 };
-
-using Circle = TCircle<long double>;
-
+ 
+using Circle = TCircle<double>;
