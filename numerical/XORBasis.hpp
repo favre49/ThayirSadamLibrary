b72cbe581fecca67d4be0fc7a36c5978cc7867a0
Rahul Prabhu <cupertinorp@gmail.com>
2021-05-02 15:58:41 +0530

    Added XOR Basis

---
+++ numerical/XORBasis.hpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// finds the xor basis of the given space
+// O(nd)
+vector<int> xorbasis(vector<int>& space) {
+  vector<int> base, elim;
+  for (int x : space) {
+    int y = x;
+    for (int b : elim)
+      y = min(y,y^b);
+    if (y) {
+      base.push_back(x);
+      elim.push_back(y);
+    }
+  }
+  return base;
+}
