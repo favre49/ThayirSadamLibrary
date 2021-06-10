ffcc0d5f6e4f1406d327c23fc18d516988345fe5
Rahul Prabhu <cupertinorp@gmail.com>
2021-06-10 12:20:24 +0530

    Updated RMQ2D to have better cache efficience, about a 30% improvement

--- data-structures/RMQ2D.hpp
+++ data-structures/RMQ2D.hpp
@@ -7,7 +7,6 @@ using namespace std;
 // Source: Me
 // Tested on: CF 1301 E
 // 2D Sparse Table, finds MAXIMUM. Just change all max to min for MIN
-// Builds in O(nmlognlogm), O(1) queries
 template<typename T>
 struct RMQ2D {
   int n,m;
@@ -20,7 +19,7 @@ struct RMQ2D {
     logs.resize(max(n,m)+1);
     int lgn = __lg(n)+1;
     int lgm = __lg(m)+1;
-    table = tensor<T,4>({n,m,lgn,lgm});
+    table = tensor<T,4>({lgn,lgm,n,m});
     for (int i = 2; i <= max(n,m); i++) {
       logs[i] = logs[i-1];
       if ((1<<(logs[i]+1)) == i)
@@ -28,16 +27,16 @@ struct RMQ2D {
     }
     for (int i =0; i < n; i++)
       for (int j = 0; j < m; j++)
-        table[{i,j,0,0}] = matrix[i][j];
+        table[{0,0,i,j}] = matrix[i][j];
     for (int k = 1; k < lgn; k++)
       for (int i = 0; i +(1<<k) <= n; i++)
         for (int j = 0; j < m; j++)
-          table[{i,j,k,0}] = max(table[{i,j,k-1,0}],table[{i+(1<<(k-1)),j,k-1,0}]);
+          table[{k,0,i,j}] = max(table[{k-1,0,i,j}],table[{k-1,0,i+(1<<(k-1)),j}]);
     for (int l = 1; l < lgm; l++)
       for (int k = 0; k < lgn; k++)
         for (int i = 0; i+(1<<k) <= n; i++)
           for (int j = 0; j + (1<<l) <= m; j++)
-            table[{i,j,k,l}] = max(table[{i,j,k,l-1}],table[{i,j+(1<<(l-1)),k,l-1}]);
+            table[{k,l,i,j}] = max(table[{k,l-1,i,j}],table[{k,l-1,i,j+(1<<(l-1))}]);
   };
 
   // Query for maximum in the submatrix with top left (r1,c1) and bottom right
@@ -46,6 +45,7 @@ struct RMQ2D {
     if (r2 < r1 || c2 < c1 || r1 < 0 || r2 >= n || c1 < 0 || c2 >= m) return -1e8;
     int a = logs[(r2-r1)+1];
     int b = logs[(c2-c1)+1];
-    return max({table[{r1,c1,a,b}],table[{r2-(1<<a)+1,c1,a,b}],table[{r1,c2-(1<<b)+1,a,b}], table[{r2-(1<<a)+1,c2-(1<<b)+1,a,b}]});
+    return max({table[{a,b,r1,c1}],table[{a,b,r2-(1<<a)+1,c1}],table[{a,b,r1,c2-(1<<b)+1}], table[{a,b,r2-(1<<a)+1,c2-(1<<b)+1}]});
   }
 };
+
