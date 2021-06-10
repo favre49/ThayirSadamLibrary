830cc2797abfd254de44fc80528c7c80ec3893fd
Rahul Prabhu <cupertinorp@gmail.com>
2021-06-10 12:12:30 +0530

    2D RMQ implementation

---
+++ data-structures/RMQ2D.hpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#include "../utility/Tensor.hpp"
+
+// Source: Me
+// Tested on: CF 1301 E
+// 2D Sparse Table, finds MAXIMUM. Just change all max to min for MIN
+// Builds in O(nmlognlogm), O(1) queries
+template<typename T>
+struct RMQ2D {
+  int n,m;
+  tensor<T,4> table;
+  vector<int> logs;
+
+  RMQ2D(vector<vector<T>>& matrix) {
+    n = matrix.size();
+    m = matrix[0].size();
+    logs.resize(max(n,m)+1);
+    int lgn = __lg(n)+1;
+    int lgm = __lg(m)+1;
+    table = tensor<T,4>({n,m,lgn,lgm});
+    for (int i = 2; i <= max(n,m); i++) {
+      logs[i] = logs[i-1];
+      if ((1<<(logs[i]+1)) == i)
+        logs[i]++;
+    }
+    for (int i =0; i < n; i++)
+      for (int j = 0; j < m; j++)
+        table[{i,j,0,0}] = matrix[i][j];
+    for (int k = 1; k < lgn; k++)
+      for (int i = 0; i +(1<<k) <= n; i++)
+        for (int j = 0; j < m; j++)
+          table[{i,j,k,0}] = max(table[{i,j,k-1,0}],table[{i+(1<<(k-1)),j,k-1,0}]);
+    for (int l = 1; l < lgm; l++)
+      for (int k = 0; k < lgn; k++)
+        for (int i = 0; i+(1<<k) <= n; i++)
+          for (int j = 0; j + (1<<l) <= m; j++)
+            table[{i,j,k,l}] = max(table[{i,j,k,l-1}],table[{i,j+(1<<(l-1)),k,l-1}]);
+  };
+
+  // Query for maximum in the submatrix with top left (r1,c1) and bottom right
+  // (r2,c2)
+  int query(int r1, int c1, int r2, int c2) {
+    if (r2 < r1 || c2 < c1 || r1 < 0 || r2 >= n || c1 < 0 || c2 >= m) return -1e8;
+    int a = logs[(r2-r1)+1];
+    int b = logs[(c2-c1)+1];
+    return max({table[{r1,c1,a,b}],table[{r2-(1<<a)+1,c1,a,b}],table[{r1,c2-(1<<b)+1,a,b}], table[{r2-(1<<a)+1,c2-(1<<b)+1,a,b}]});
+  }
+};
