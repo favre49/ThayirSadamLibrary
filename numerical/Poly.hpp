37bda53543ddec3950df9b0ed7df19f53056befa
Rahul Prabhu <cupertinorp@gmail.com>
2021-08-02 14:37:23 +0530

    improved polynomial library

--- numerical/Poly.hpp
+++ numerical/Poly.hpp
@@ -1,107 +1,146 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Source: Me
-// Tested On:
+// Source: me
+// Tested on: CF 1548 C
+// Polynomial library
+// Many improvements possible
 template <typename T>
-struct poly {
-  // The coefficients of the polynomial in increasing order of degree
-  vector<T> a;
+struct Polynomial : vector<T> {
+  using vector<T>::vector;
+  using P = Polynomial;
 
-  // Constructors
-  poly(){}
-  poly(vector<T> t):a(t) {}
-
-  // Operations
-  poly operator += (const poly& t) {
-    a.resize(max(a.size(),t.a.size()));
-    for (int i = 0; i< t.a.size(); i++)
-      a[i] += t.a[i];
+  P& operator+=(const P& r) {
+    if (r.size() > this->size()) this->resize(r.size());
+    for (int i = 0; i < (int)r.size(); i++)
+      (*this)[i] += r[i];
     return *this;
   }
 
-  poly operator -= (const poly& t) {
-    a.resize(max(a.size(),t.a.size()));
-    for (int i = 0; i< t.a.size(); i++)
-      a[i] -= t.a[i];
+  P& operator+=(const T& r) {
+    if (this->empty()) this->resize(1);
+    (*this)[0] += r;
     return *this;
   }
 
-  poly operator *= (const poly& t) {
-    vector<T> ans(a.size()+t.a.size()-1);
-    for (int i = 0; i < (int)a.size(); i++)
-      for (int j = 0; j < (int)t.a.size(); j++)
-        ans[i+j] += a[i]*t.a[j];
-    a = ans;
+  P& operator-=(const P& r) {
+    if (r.size() > this->size()) this->resize(r.size());
+    for (int i = 0; i < (int)r.size(); i++)
+      (*this)[i] -= r[i];
     return *this;
   }
 
-  poly operator *= (T t) {
-    for (auto& x : a)
-      x *= t;
+  P& operator-=(const T& r) {
+    if (this->empty()) this->resize(1);
+    (*this)[0] -= r;
+    return *this;
+  }
+  
+  P& operator*= (const P& r) {
+    vector<T> res(this->size() + r.size()-1);
+    for (int i = 0; i < this->size(); i++)
+      for (int j = 0; j < r.size(); j++)
+        res[i+j] += (*this)[i]*r[j];
+    return *this = P(res.begin(),res.end());
+  }
+
+  P& operator*= (const T& r) {
+    for (int i = 0; i < this->size(); i++) (*this)[i] *= r;
     return *this;
   }
 
-  poly operator /= (T d) {
-    for (auto& x : a)
-      x /= d;
+  P& operator/= (const P& r) {
+    if (this->size() < r.size()) {
+      this->clear();
+      return *this;
+    }
+    int n = this->size() - r.size() + 1;
+    P f(*this), g(r);
+    g.shrink();
+    T coeff = 1/g.back();
+    for (auto& x : g) x *= coeff;
+    int deg = (int)f.size() - (int)g.size() + 1;
+    int gs = g.size();
+    P quo(deg);
+    for (int i = deg-1; i >= 0; i--) {
+      quo[i] = f[i+gs-1];
+      for (int j = 0; j < gs; j++)
+        f[i+j] -= quo[i] * g[j];
+    }
+    *this = quo*coeff;
+    this->resize(n,T(0));
     return *this;
   }
 
-  T operator() (T x) {
+  P& operator %=(const P& r) {
+    *this -= *this/r*r;
+    shrink();
+    return *this;
+  }
+
+  P operator+(const P& r) const { return P(*this) += r; }
+  P operator+(const T r) const { return P(*this) += r; }
+  P operator-(const P& r) const { return P(*this) -= r; }
+  P operator-(const T r) const { return P(*this) -= r; }
+  P operator*(const P& r) const { return P(*this) *= r; }
+  P operator*(const T r) const { return P(*this) *= r; }
+  P operator/(const P& r) const { return P(*this) /= r; }
+  P operator%(const P& r) const { return P(*this) %= r; }
+
+  void shrink() {
+    while (this->size() && this->back() == T(0))
+      this->pop_back();
+  }
+
+  // Differentiate
+  P diff() const {
+    const int n = (int)this->size();
+    P ret(max(0,n-1));
+    for (int i = 1; i < n; i++)
+      ret[i-1] = (*this)[i]*i;
+    return ret;
+  }
+
+  // Integrate
+  // Can make it faster by finding inverses faster
+  P integral() const {
+    const int n = (int)this->size();
+    P ret(n+1);
+    ret[0] = 0;
+    for (int i = 0; i < n; i++)
+      ret[i+1] = (*this)[i]/T(i+1);
+    return ret;
+  }
+
+  T eval(T x) const {
     T res = 0, cum = 1;
-    for (int i = 0; i < a.size(); i++) {
-      res += a[i]*cum;
+    for (int i = 0; i < this->size(); i++) {
+      res += (*this)[i]*cum;
       cum *= x;
     }
     return res;
   }
-
-  poly operator + (const poly& t) const {return poly(*this) += t;}
-  poly operator - (const poly& t) const {return poly(*this) -= t;}
-  poly operator * (const poly& t) const {return poly(*this) *= t;}
-  poly operator * (const T t) const {return poly(*this) *= t;}
-  poly operator / (T t) const {return poly(*this) /= t;}
-
-  // Integrate polynomial
-  poly integrate() {
-    vector<T> res = {0};
-    for (int i = 0; i < a.size(); i++)
-      res.push_back(a[i]/T(i+1));
-    return res;
-  }
-
-  // Differentiate polynomial
-  poly deriv() {
-    vector<T> res;
-    for (int i = 1; i < a.size(); i++)
-      res.push_back(a[i]*T(i));
-    return res;
-  }
-
-  // Integrate polynomial over [l,r]
-  T integrate_val(int l, int r) {
-    poly ans = integrate();
-    return ans(r)-ans(l);
-  }
-
-  // Interpolate using Lagrangian interpolation
-  void interpolate(vector<pair<int,T>> pts) {
-    poly<T> ret;
-    for (int i = 0; i < (int)pts.size(); i++) {
-      poly<T> prod({1});
-      T todiv = 1;
-      for (int j = 0; j < (int)pts.size(); j++) {
-        if (i == j) continue;
-        todiv *= (pts[i].first - pts[j].first);
-        vector<T> tmpv({-pts[j].first,1});
-        poly<T> tmp(tmpv);
-        prod *= tmp;
-      }
-      prod /= todiv;
-      prod *= pts[i].second;
-      ret += prod;
-    }
-    *this = ret;
-  }
 };
+
+// Interpolate using Lagrangian interpolation
+// Input in the form (x,f(x))
+template<typename T> 
+Polynomial<T> interpolate(vector<pair<int,T>> pts) {
+  Polynomial<T> ret;
+  for (int i = 0; i < (int)pts.size(); i++) {
+    Polynomial<T> prod({1});
+    T todiv = 1;
+    for (int j = 0; j < (int)pts.size(); j++) {
+      if (i==j) continue;
+      todiv *= (pts[i].first-pts[j].first);
+      Polynomial<T> tmp({-pts[j].first,1});
+      prod *= tmp;
+    }
+    prod /= todiv;
+    prod *= pts[i].second;
+    ret += prod;
+  }
+  return ret;
+}
+
+
