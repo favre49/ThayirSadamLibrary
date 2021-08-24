80e771d14368774ff46758da4ab7c931ad8b3f27
Rahul Prabhu <cupertinorp@gmail.com>
2021-08-24 18:50:30 +0530

    New Matrix library from Nyaan, just because it's neater

--- numerical/Matrix.hpp
+++ numerical/Matrix.hpp
@@ -1,103 +1,126 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Source: Me
+using ll = long long;
+
+// Source: Nyaan
 // Tested On:
 // Matrix library
 // Use M^k to raise to a power
 template<class T>
-struct Mat{
-  int n,m;
-  vector<vector<T>> d;
+struct Matrix{
+  vector<vector<T>> A;
 
-  Mat(): n(0), m(0){}
+  Matrix() = default;
+  Matrix(int n):A(n,vector<T>(n,T())) {}
+  Matrix(int n, int m):A(n,vector<T>(m,T())) {}
 
-  Mat(int _n, int _m): n(_n), m(_m) {
-    d = vector<vector<T>>(n,vector<T>(m));
+  int H() const { return A.size(); }
+  int W() const { return A[0].size(); }
+
+  vector<T>& operator[] (int index) { return A[index]; }
+  const vector<T>& operator[] (int index) const { return A[index]; }
+
+  static Matrix I(int n) {
+    Matrix id(n);
+    for (int i =0; i < n; i++) id[i][i] = 1;
+    return id;
   }
 
-  Mat(const vector<vector<T>>& v): Mat((int)v.size(),(int)v[0].size()){
+  Matrix& operator+=(const Matrix& B) {
+    int n = H(), m = W();
+    assert(n == B.H() && m == B.W());
     for (int i = 0; i < n; i++)
       for (int j = 0; j < m; j++)
-        d[i][j] = v[i][j];
+        (*this)[i][j] += B[i][j];
+    return *this;
   }
 
-  vector<T>& operator[] (int index) {
-    return d[index];
-  }
-
-  Mat operator +(Mat other) const {
-    assert(n == other.n && m == other.m);
-    Mat res = Mat(n,m);
+  Matrix& operator-=(const Matrix& B) {
+    int n = H(), m = W();
+    assert(n == B.H() && m == B.W());
     for (int i = 0; i < n; i++)
       for (int j = 0; j < m; j++)
-        res.d[i][j] = d[i][j] + other.d[i][j];
-    return res;
+        (*this)[i][j] -= B[i][j];
+    return *this;
+  }
+  
+  Matrix& operator*=(const Matrix& B) {
+    int n = H(), m = W();
+    int bn = B.H(), bm = B.W();
+    assert(m == bn);
+    vector<vector<T>> C(n,vector<T>(bm,T()));
+    for (int i = 0; i < n; i++)
+      for (int k = 0; k < m; k++)
+        for (int j = 0; j < bm; j++)
+          C[i][j] += (*this)[i][k]*B[k][j];
+    A.swap(C);
+    return *this;
   }
 
-  Mat operator -(Mat other) const {
-    assert(n == other.n && m == other.m);
-    Mat res = Mat(n,m);
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < m; j++)
-        res.d[i][j] = d[i][j] - other.d[i][j];
-    return res;
-  }
-
-  Mat operator *(Mat other) const {
-    assert(m == other.n);
-    Mat prod = Mat(n,other.m);
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < m; j++)
-        for (int k = 0; k < other.m; k++)
-          prod.d[i][k] += d[i][j] * other.d[j][k];
-    return prod;
-  }
-
-  Mat operator ^(long long p)
-  {
-    assert(p >= 0);
-    Mat res(n,n), base(*this);
-    for (int i = 0; i < n; i++)
-      res.d[i][i] = 1;
-    for (;p; p /= 2) {
-      if (p&1) res = res*base;
-      base = base*base;
+  Matrix& operator^=(ll k) {
+    Matrix B = Matrix::I((*this).H());
+    while(k > 0) {
+      if (k&1) B *= *this;
+      *this *= *this;
+      k >>= 1LL;
     }
-    return res;
+    A.swap(B.A);
+    return *this;
+  }
+
+  Matrix operator+(const Matrix& B) const { return (Matrix(*this) += B); }
+  Matrix operator-(const Matrix& B) const { return (Matrix(*this) -= B); }
+  Matrix operator*(const Matrix& B) const { return (Matrix(*this) *= B); }
+  Matrix operator^(const ll k) const { return (Matrix(*this) ^= k); }
+
+  bool operator==(const Matrix& B) const {
+    assert(H() == B.H() && W() == B.W());
+    for (int i =0 ; i < H(); i++)
+      for (int j = 0; j < W(); j++)
+        if (A[i][j] != B[i][j]) return false;
+    return true;
+  }
+
+  bool operator!=(const Matrix& B) const {
+    assert(H() == B.H() && W() == B.W());
+    for (int i =0 ; i < H(); i++)
+      for (int j = 0; j < W(); j++)
+        if (A[i][j] != B[i][j]) return true;
+    return false;
   }
 
   // Get the determinant of the matrix in O(N^3)
+  // Remember to make all n into n-1 when using with Kirchhoff's theorem
   T det() {
+    int n = H(), m = W();
     assert(n == m);
+    Matrix B(*this);
     T prod = 1;
-    Mat st(n,n);
-    st.d = d;
-    auto elim = [this](int a, int c){
-      T x = d[c][a];
-      for (int i = a; i < n; i++)
-        d[c][i] -= x*d[a][i];
-    };
-    for (int i = 0; i < n; i++){
-      bool done = false;
-      for (int j = i; j < n; j++){
-        if (d[j][i] != 0) {
-          done = 1;
-          if (j != i)
-            swap(d[j], d[i]);
-          if ((j-i)%2==1)
-            prod *= -1;
-          prod = prod*d[i][i];
-          T x = d[i][i].inv();
-          for (int k = i; k < n; k++)
-            d[i][k] *= x;
-          for (int k = i+1; k < n; k++)
-            elim(i,k);
+    for (int i =0 ; i < n; i++) {
+      int idx = -1;
+      for (int j = i; j < n; j++) {
+        if (B[j][i] != 0) {
+          idx = j;
+          break;
         }
       }
-      if (!done) return 0;
+      if (idx == -1) return 0;
+      if (i != idx) {
+        prod *= T(-1);
+        swap(B[i],B[idx]);
+      }
+      prod *= B[i][i];
+      T inv = T(1)/B[i][i];
+      for (int j =0 ; j < n; j++)
+        B[i][j] *= inv;
+      for (int j = i+1; j < n; j++) {
+        T a = B[j][i];
+        if (a==0) continue;
+        for (int k = i; k < m; k++)
+          B[j][k] -= B[i][k]*a;
+      }
     }
-    d = st.d;
     return prod;
   }
 };
