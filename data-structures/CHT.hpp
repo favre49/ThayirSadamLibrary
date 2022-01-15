67cdbca9a61cc449e0203b6fe5b609b19d9f0ffc
Rahul Prabhu <cupertinorp@gmail.com>
2022-01-15 12:20:14 +0530

    Rewrote Dynamic CHT

--- data-structures/CHT.hpp
+++ data-structures/CHT.hpp
@@ -1,42 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = int64_t;
-
 // Source: KACTL
-// Tested on: Yosupo Line Add Get Min
-// Add lines of the form kx+m and query the MAXIMUM at x
-bool Q;
-struct Line {
-  mutable ll k, m, p;
-  bool operator<(const Line& o) const {
-    return Q ? p < o.p : k < o.k;
-  }
+// Tested on: Yosupo Judge Line Add Get Min
+// Dynamic Convex Hull Trick, with two operations:
+// 1. Insert line kx+m
+// 2. Query maximum value at x
+// TODO: Implementation using doubles via template enable_ifs
+namespace cht_internal {
+  struct Line {
+    mutable int64_t k,m,p;
+    
+    bool operator < (const Line& o) const {
+      return k < o.k;
+    }
+
+    bool operator < (const int64_t x) const {
+      return p < x;
+    }
+  };
 };
 
-struct LineContainer : multiset<Line> {
-  // (for doubles, use inf = 1/.0, div(a,b) = a/b)
-  const ll inf = LLONG_MAX;
-  ll div(ll a, ll b) { // floored division
-    return a / b - ((a ^ b) < 0 && a % b); }
-  bool isect(iterator x, iterator y) {
-    if (y == end()) { x->p = inf; return false; }
-    if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
-    else x->p = div(y->m - x->m, x->k - y->k);
-    return x->p >= y->p;
-  }
-  // Add line kx+m
-  void add(ll k, ll m) {
-    auto z = insert({k, m, 0}), y = z++, x = y;
-    while (isect(y, z)) z = erase(z);
-    if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
-    while ((y = x) != begin() && (--x)->p >= y->p)
-      isect(x, erase(y));
-  }
-  // Query maximum at x
-  ll query(ll x) {
-    assert(!empty());
-    Q = 1; auto l = *lower_bound({0,0,x}); Q = 0;
-    return l.k * x + l.m;
-  }
+struct CHT : multiset<cht_internal::Line,less<>> {
+  public:
+    // Add line kx+m
+    void add(int64_t k, int64_t m) {
+      auto z = insert({k,m,0}), y = z++, x = y;
+      while(isect(y,z))
+        z = erase(z);
+      if (x != begin() && isect(--x,y))
+        isect(x,y = erase(y));
+      while((y = x) != begin() && (--x)->p >= y->p)
+        isect(x,erase(y));
+    }
+
+    // Query maximum
+    int64_t query(int64_t x) {
+      auto l = *lower_bound(x);
+      return l.k * x + l.m;
+    }
+
+  private:
+    const int64_t INF = numeric_limits<int64_t>::max();
+
+    int64_t div(int64_t a, int64_t b) {
+      return a/b - ((a^b) < 0 && a%b);
+    }
+
+    bool isect(iterator x, iterator y) {
+      if (y == end()) {
+        x->p = INF;
+        return false;
+      }
+      if (x->k == y->k)
+        x->p = x->m > y-> m ? INF : -INF;
+      else 
+        x->p = div(y->m - x->m, x->k - y->k);
+      return x->p >= y->p;
+    }
 };
+
+
