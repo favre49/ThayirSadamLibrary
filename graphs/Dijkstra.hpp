4b3e2942344c82cb78a54fe5a57a1679f1f00006
Rahul Prabhu <cupertinorp@gmail.com>
2022-01-18 19:56:26 +0530

    Added path() and better naming for Dijkstra

--- graphs/Dijkstra.hpp
+++ graphs/Dijkstra.hpp
@@ -5,37 +5,47 @@ using namespace std;
 // Tested on : Yosupo Shortest Path
 // Use __gnu_pbds::priority_queue to get tiny speed increase
 struct Dijkstra {
-  using ll = int64_t;
-  using dist_type = pair<ll,int>;
+  using dist_type = pair<int64_t,int>;
   int n;
-  const ll INF = 1e18;
-  vector<ll> d;
+  const int64_t INF = 1e18;
+  vector<int64_t> dist;
   vector<int> pred;
-  vector<vector<pair<int,ll>>> adj;
+  vector<vector<pair<int,int64_t>>> adj;
 
-  Dijkstra(int _n):n(_n) {
-    d.resize(n); pred.resize(n); adj.resize(n);
-  }
-  void add_edge(int u, int v, ll w) {
+  Dijkstra(int n):n(n), dist(n), pred(n), adj(n) {}
+
+  void add_edge(int u, int v, int64_t w) {
     adj[u].push_back({v,w});
   }
-  void dijkstra(int s) {
-    for (int i = 0; i< n; i++) pred[i] = -1, d[i] = INF; // reset
-    d[s] = 0;
+
+  void build(int s) {
+    for (int i = 0; i < n; i++) // reset
+      pred[i] = -1, dist[i] = INF;
+    dist[s] = 0;
     priority_queue<dist_type,vector<dist_type>,greater<dist_type>> q;
     q.push({0,s});
     while(!q.empty()) {
-      auto [d_v,v] = q.top();
+      auto [d_v, v] = q.top();
       q.pop();
-      if (d[v] != d_v) continue;
-      for (auto [to,len] : adj[v]) {
-        if (d[v]+len < d[to]) {
-          d[to] = d[v]+len;
+      if (dist[v] != d_v) continue;
+      for (auto& [to, len] : adj[v]) {
+        if (dist[v] + len < dist[to]) {
+          dist[to] = dist[v] + len;
           pred[to] = v;
-          q.push({d[to],to});
+          q.push({dist[to],to});
         }
       }
     }
   }
-};
 
+  vector<int> path(int goal) const {
+    if (dist[goal] == INF) return {};
+    vector<int> ans = {goal};
+    while(pred[goal] != -1) {
+      goal = pred[goal];
+      ans.push_back(goal);
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+  }
+};
