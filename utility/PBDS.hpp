39da7dc974c7d026c75f976ff45b597c88617dea
Rahul Prabhu <cupertinorp@gmail.com>
2022-02-24 00:32:30 +0530

    Converted interval container into a struct

--- utility/PBDS.hpp
+++ utility/PBDS.hpp
@@ -1,7 +1,8 @@
 #include <bits/extc++.h>
 using namespace std;
 
-// Templates for using PBDS structures
+// Source: ecnerwala
+// Templates for Policy Based Data Structures
 struct splitmix64_hash {
   static uint64_t splitmix64(uint64_t x) {
     // http://xorshift.di.unimi.it/splitmix64.c
