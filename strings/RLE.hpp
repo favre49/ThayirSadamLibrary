e00d914a2aac33d97472ff93696be3ec53a83669
Rahul Prabhu <cupertinorp@gmail.com>
2022-05-15 20:39:46 +0530

    Run Length Encoding and easy compression

---
+++ strings/RLE.hpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Source: ???
+// Find the run length encoding of a string or array
+template <typename T = char, typename U>
+vector<pair<T,int>> run_length_encoding(const U& s) {
+  int n = s.size();
+  vector<pair<T, int>> res;
+  if (n == 0) return res;
+  T st = s.front();
+  int cnt = 1;
+  for (int i = 1; i < n; i++) {
+    if (s[i] != st) {
+      res.push_back({st, cnt});
+      cnt = 0;
+    }
+    st = s[i];
+    cnt++;
+  }
+  res.push_back({st,cnt});
+  return res;
+}
---
+++ utility/Compress.hpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Source: Nyaan
+// Compress a set of unbounded values into the range [0, n)
+template <class T>
+struct Compress {
+  vector<T> compress;
+
+  Compress(const vector<T>& v) {
+    compress = v;
+    sort(compress.begin(),compress.end());
+    compress.erase(unique(compress.begin(),compress.end()), compress.end());
+  }
+
+  int get(const T& x) const {
+    return lower_bound(compress.begin(),compress.end(),x) - compress.begin();
+  }
+  int size() const { return compress.size(); }
+
+  int operator()(const T& x) const { return get(x); }
+  T operator[] (int idx) { return compress[idx]; }
+};
